18dbd8a7389932a253ee687553b82733f87f5dc0
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2018-09-30 23:13:01 -0400

    Changed -match to -like and fixed syntax. Added if statement in case no normal silence detected.

--- noise_reduction.ps1
+++ noise_reduction.ps1
@@ -16,68 +16,80 @@ if ($right_location -eq "n")
     {
         write-host "Put this script in the same directory as your recordings, then execute it."
     }
-    else
-        {
-            # Prompt the user for the file extension of their recordings
-            $ext = read-host "What is the file extension of the recordings you're working with?"
+else
+    {
+        # Prompt the user for the file extension of their recordings
+        $ext = read-host "What is the file extension of the recordings you're working with?"
 
-            # Get a list of recordings.
-            $allrecordings = get-childitem -name *.$ext
+        # Get a list of recordings.
+        $allrecordings = get-childitem -name *.$ext
 
-            # Create a subdirectory where cleaned recordings will be moved and tell the user to wait
-            new-item -name Cleaned_Recordings -itemtype directory
-            write-host "Please wait..."
+        # Create a subdirectory where cleaned recordings will be moved and tell the user to wait
+        new-item -name Cleaned_Recordings -itemtype directory
+        write-host "Please wait..."
 
-            foreach ($recording in $allrecordings)
-                {
-                    # Detect absolute silence
-                    ffmpeg -i $recording -af silencedetect=n=0.01:d=1 -f null - > "absolute_silences.txt" 2>&1
+        foreach ($recording in $allrecordings)
+            {
+                # Detect absolute silence
+                ffmpeg -i $recording -af silencedetect=n=0.01:d=1 -f null - > "absolute_silences.txt" 2>&1
         
-                    # Was there absolute silence?
-                    if ("absolute_silences.txt" -match "start_silence")
-                        {
-                            # If yes, keep add the file name to a log.
-                            $recording >> "recordings_to_do_manually.txt"
-                        }
-                        else
-                        {
-                            # If not, detect normal silence
-                            ffmpeg -i $recording -af silencedetect=n=0.1:d=1 -f null - > "normal_silences.txt" 2>&1
-                
-                            # Store the first normal silence start time
-                            $silence_start = (select-string -path "normal_silences.txt" -pattern "silence_start: (.*)$").Matches.Groups[1].Value
-                
-                            # Store the first normal silence end time
-                            $silence_end = (select-string -path "normal_silences.txt" -pattern "silence_end: (.*) \| silence_duration: (.*)$").Matches.Groups[1].Value
-
-                            # Trim the normal silence and save it to a new audio file
-                            sox $recording normal_silence.$ext trim $silence_start =$silence_end
-
-                            # Create noise profile from the normal silence audio file
-                            sox normal_silence.$ext -n noiseprof temp.noise-profile
-
-                            # Apply noise reduction to the recording using the noise profile
-                            sox $recording cleaned_$recording noisered temp.noise-profile 0.3
-
-                            # Creates moves all the cleaned recordings to a subdirectory called Cleaned_Recordings
-                            move-item -path "cleaned_$recording" -destination ".\Cleaned_Recordings"
-                        }
-                }
-            # Cleans up extraneous files
-            Remove-Item *_silences.txt, normal_silence.$ext, temp.noise-profile
-
-            # Print result messages
-            get-childitem -name > directory_contents.txt
-            if ("directory_contents.txt" -match "recordings_to_do_manually.txt")
-                {
-                    write-host "Your cleaned recordings are in \Cleaned_Recordings."
-                    write-host "Don't forget to manually apply noise reduction to the files in recordings_to_do_manually.txt!"
-                }
+                # Was there absolute silence?
+                $absolute_silence_check = get-content absolute_silences.txt
+                if ($absolute_silence_check -like "*silence_start*")
+                    {
+                        # If yes, add the filename to a log.
+                        $recording >> "recordings_to_do_manually.txt"
+                    }
                 else
                     {
-                        write-host "All recordings cleaned!"
-                        write-host "They can be found in .\Cleaned_Recordings."
+                        # If not, detect normal silence
+                        ffmpeg -i $recording -af silencedetect=n=0.1:d=1 -f null - > "normal_silences.txt" 2>&1
+                
+                        # Make sure some normal silence was detected
+                        $normal_silence_check = get-content normal_silences.txt
+                        if ($normal_silence_check -like "*silence_start*")
+                            {
+                                # Store the first normal silence start time
+                                $silence_start = (select-string -path "normal_silences.txt" -pattern "silence_start: (.*)$").Matches.Groups[1].Value
+            
+                                # Store the first normal silence end time
+                                $silence_end = (select-string -path "normal_silences.txt" -pattern "silence_end: (.*) \| silence_duration: (.*)$").Matches.Groups[1].Value
+
+                                # Trim the normal silence and save it to a new audio file
+                                sox $recording normal_silence.$ext trim $silence_start =$silence_end
+
+                                # Create noise profile from the normal silence audio file
+                                sox normal_silence.$ext -n noiseprof temp.noise-profile
+
+                                # Apply noise reduction to the recording using the noise profile
+                                sox $recording cleaned_$recording noisered temp.noise-profile 0.3
+
+                                # Creates moves all the cleaned recordings to a subdirectory called Cleaned_Recordings
+                                move-item -path "cleaned_$recording" -destination ".\Cleaned_Recordings"
+                            }
+                        else
+                            {
+                                # Add filename to log to do manually
+                                $recording >> "recordings_to_do_manually.txt"
+                            }
                     }
-            # Final cleanup
-            remove-item directory_contents.txt
-        }
+            }
+        # Cleans up extraneous files
+        Remove-Item *_silences.txt, normal_silence.$ext, temp.noise-profile
+
+        # Print result messages
+        get-childitem -name > directory_contents.txt
+        $recordings_to_do_manually_check = get-content directory_contents.txt
+        if ($recordings_to_do_manually_check -like "*recordings_to_do_manually*")
+            {
+                write-host "Your cleaned recordings are in .\Cleaned_Recordings."
+                write-host "Check recordings_to_do_manually.txt for files that need to be done manually."
+            }
+        else
+            {
+                write-host "All recordings cleaned!"
+                write-host "They can be found in .\Cleaned_Recordings."
+            }
+        # Final cleanup
+        remove-item directory_contents.txt
+    }
